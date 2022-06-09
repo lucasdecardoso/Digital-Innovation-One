a7055eb90caace211aee6d2abd8bf183edeba384
Lucas Cardoso <lucasc45@gmail.com>
2022-06-08 21:56:12 -0300

    Update multiplicacaoSimples.cs

--- Desafios de Código/multiplicacaoSimples.cs
+++ Desafios de Código/multiplicacaoSimples.cs
@@ -16,7 +16,7 @@ using System;
             int a, b;
             a = Convert.ToInt32(Console.ReadLine());
             b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("PROD = " + (a * b)); //implemente o código que representa a multiplicação.
+            Console.WriteLine("PROD = " + (a * b)); 
             Console.ReadLine();
         }
     }
